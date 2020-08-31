607b8964196e2b3a90af08b8025ffe7f5f8c9cf9
Nanda Adi <nandaadis186@gmail.com>
2020-08-31 20:51:12 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/travada/sampeldata/dummy_berita.kt
@@ -0,0 +1,7 @@
+package com.example.travada.sampeldata
+
+object dummy_berita {
+
+
+
+}
---
+++ app/src/main/java/com/example/travada/util/util.kt
@@ -0,0 +1,8 @@
+package com.example.travada.util
+
+object util {
+    const val SF = "shared_preference"
+    private const val BASE_URL = ""
+
+
+}
