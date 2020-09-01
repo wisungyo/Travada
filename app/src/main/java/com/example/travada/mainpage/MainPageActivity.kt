5192ab858978f0c8cf2b584e3c9b782500e0e4a8
wisungyo <wisungyo@gmail.com>
2020-09-02 00:46:23 +0700

    bottom navigation done

--- app/src/main/java/com/example/travada/mainpage/MainPageActivity.kt
+++ app/src/main/java/com/example/travada/mainpage/MainPageActivity.kt
@@ -1,13 +1,12 @@
-package com.example.travada
+package com.example.travada.mainpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.travada.R
 
-class MainActivity : AppCompatActivity() {
+class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_main_page)
     }
 }
