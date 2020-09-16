204a09ea440ab54e776a559b1c049d19d670de76
Hanif yogatama <hanifyotama@gmail.com>
2020-09-16 15:35:18 +0700

    add detail rencana

--- app/src/main/java/com/example/travada/features/rencana/detailrencana/DetailRencanaActivity.kt
+++ app/src/main/java/com/example/travada/features/rencana/detailrencana/DetailRencanaActivity.kt
@@ -2,11 +2,15 @@ package com.example.travada.features.rencana.detailrencana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.travada.R
+import kotlinx.android.synthetic.main.activity_detail_rencana.*
+import kotlinx.android.synthetic.main.activity_tabungan.*
 
 class DetailRencanaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_rencana)
+        nestedView.overScrollMode = View.OVER_SCROLL_NEVER
     }
 }
