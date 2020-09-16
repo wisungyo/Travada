7033b389f03695d68f56f6dde58d4e90b90e5461
Hanif yogatama <hanifyotama@gmail.com>
2020-09-16 09:49:05 +0700

    add detail rencana

---
+++ app/src/main/java/com/example/travada/features/rencana/detailrencana/DetailRencanaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.travada.features.rencana.detailrencana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.travada.R
+
+class DetailRencanaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_rencana)
+    }
+}
