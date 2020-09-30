6cb0b8652066aca7d3d28929b393cc34b0b75eae
Hanif yogatama <hanifyotama@gmail.com>
2020-10-01 05:46:14 +0700

    resolve travasave

--- app/src/main/java/com/example/travada/features/tabungan/formtabungansatu/FormTabunganOnePresenter.kt
+++ app/src/main/java/com/example/travada/features/tabungan/formtabungansatu/FormTabunganOnePresenter.kt
@@ -1,17 +1,31 @@
 package com.example.travada.features.tabungan.formtabungansatu
-
 class FormTabunganOnePresenter(private val listener: Listener)  {
+    fun checked(
+        tujuan: String,
+        jumlah: String,
+        uriGambar: String
+    ) {
 
-    fun checked(tujuan: String, jumlah: String) {
-        if (tujuan.isNotEmpty() && jumlah.isNotEmpty()) {
+        if (tujuan.isNotEmpty() && jumlah.isNotEmpty() && uriGambar.isNotEmpty()) {
             listener.btnActive()
         } else {
             listener.btnInactive()
         }
     }
 
+    // TODO : pengerjaan menunggu API
+//    fun nextPage() {
+//        listener.goToNextPage()
+//    }
+
+
     interface Listener {
         fun btnActive()
         fun btnInactive()
+        //      fun goToNextPage()
+        fun errTujuan(message: String?)
+        fun errJumlah(message: String?)
+        fun goToUploadGambar()
+        fun goToUpdateGambar()
     }
 }
