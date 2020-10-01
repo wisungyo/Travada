6b12f096d4d0650b77fa0cb566215b85f29f5954
wisungyo <wisungyo@gmail.com>
2020-10-01 12:14:47 +0700

    screen pilih saldo layout DONE

---
+++ app/src/main/java/com/example/travada/detailriwayat/view/BayarCicilanActivity.kt
@@ -0,0 +1,12 @@
+package com.example.travada.detailriwayat.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.travada.R
+
+class BayarCicilanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bayar_cicilan)
+    }
+}
