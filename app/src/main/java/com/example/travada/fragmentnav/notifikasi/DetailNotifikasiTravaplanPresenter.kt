54230a364902dc7d5eb15f5ad70b5805228eec0e
Hanif yogatama <hanifyotama@gmail.com>
2020-10-09 07:12:10 +0700

    notifikasi

---
+++ app/src/main/java/com/example/travada/fragmentnav/notifikasi/DetailNotifikasiTravaplanPresenter.kt
@@ -0,0 +1,4 @@
+package com.example.travada.fragmentnav.notifikasi
+
+class DetailNotifikasiTravaplanPresenter {
+}
--- app/src/main/java/com/example/travada/fragmentnav/notifikasi/DetailNotifikasiTravasave.kt
+++ app/src/main/java/com/example/travada/fragmentnav/notifikasi/DetailNotifikasiTravasave.kt
@@ -3,10 +3,25 @@ package com.example.travada.fragmentnav.notifikasi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.travada.R
+import com.example.travada.fragmentnav.notifikasi.model.DataNotifikasi
+import kotlinx.android.synthetic.main.activity_detail_notifikasi_travasave.*
 
-class DetailNotifikasiTravasave : AppCompatActivity() {
+class DetailNotifikasiTravasave : AppCompatActivity(),DetailNotifikasiTravasavePresenter.Listener {
+    private lateinit var presenter : DetailNotifikasiTravasavePresenter
+    private lateinit var data : DataNotifikasi
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_notifikasi_travasave)
+        presenter = DetailNotifikasiTravasavePresenter(this)
+        intent.getParcelableExtra<DataNotifikasi>("notifikasi")?.let {
+            data = it
+        }
+
+        fetchNotifikasiData()
+    }
+
+    private fun fetchNotifikasiData(){
+        tvDescNotifikasi.text= data.pesan
     }
 }
